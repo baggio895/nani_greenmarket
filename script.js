acb47e5a095da942098f244d4249fcf9d49d7446
baggio895 <baggio.chang@ybpc.org>
2026-08-14 15:17:04 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.menu-toggle');
+const nav = document.querySelector('#primary-nav');
+menuButton.addEventListener('click', () => {
+  const open = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', String(!open));
+  nav.classList.toggle('open', !open);
+});
